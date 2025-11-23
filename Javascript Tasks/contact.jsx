ce7316dab9a27246ae1809acc76b7e0636da073c
Jhanani22 <jhanani2201@gmail.com>
2025-11-23 17:02:06 +0530

    Delete Javascript Tasks directory

--- Javascript Tasks/contact.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Contact() {
-  return <h2>This is the Contact Page</h2>;
-}
-
-export default Contact;
--- Javascript Tasks/home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Home() {
-  return <h2>This is the Home Page</h2>;
-}
-
-export default Home;
